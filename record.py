d4d0075f804970bc7d51012dde4460d7da236cf5
pasdesignal <noiseisgood@gmail.com>
2018-01-27 22:33:06 +1300

    Added basic wav folder watchdog

--- record.py
+++ record.py
@@ -5,14 +5,12 @@ import datetime
 import os
 
 ##TO DO:
-#housekeeping before and after
+#improve logging to file (include ffmpeg stdout and errors)
 #folder watchdog process to process wav file - trim to silence (start and between 02:50mins - 03:10mins )
-#folder watchdog process trimmed .wav file to opus
+#folder watchdog transcode trimmed .wav file to opus
 #folder watchdog process to ftp/scp ogg/opus file to ELF(?)
 #setup cron jobs to call above jobs each hour two seconds before the hour
-#log all output to file
 #create test script to test all of above at any moment
-#try/except clause
 
 wav_dir = '/home/rnzweb/audio/wav/'
 opus_dir = '/home/rnzweb/audio/opus/'
@@ -23,7 +21,7 @@ filename = 'rnznews_'+date_time+'.wav'
 def record():
 	print "initiating ffmpeg record of livewire stream"
 	ff = ffmpy.FFmpeg(global_options='-v debug',inputs={sdp_file : '-c:a pcm_s24be -r:a 48000 -ac 2 -t 30'},outputs={(wav_dir+filename) : None })
-	print ff
+	print ff.cmd
 	ff.run()
 
 def housekeeping():
@@ -40,12 +38,12 @@ def housekeeping():
 
 if __name__ == '__main__':
 	try:
-		housekeeping()
-		rec_output = record() 
+		print "filename:", filename
+		#housekeeping()   #this should not be at the start of this script it will slow down start of recording
+		record()
 	except KeyboardInterrupt:
 		print "manually interrupted!"
 	except Exception as e:
 		print("Error:%s".format(e))
 	finally:
-		print rec_output
-		housekeeping()
+		pass
---
+++ watchdog_transcode.py
@@ -0,0 +1,46 @@
+
+from watchdog.events import PatternMatchingEventHandler  
+from watchdog.observers import Observer
+import os
+import datetime
+
+wav_dir = '/home/rnzweb/audio/wav/'
+opus_dir = '/home/rnzweb/audio/opus/'
+sdp_file = '/home/rnzweb/news_recorder/rnz_national.sdp'
+date_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+filename = 'rnznews_'+date_time+'.wav'
+
+class MyHandler(PatternMatchingEventHandler):
+	
+	patterns = ["*.wav"]
+
+##This needs to be improved to tolerate empty/bad xmls...
+	def process(self, event):
+		print event.src_path, event.event_type         #debug
+		#everything here is what happens once the event is triggered
+		_files = os.listdir(event.src_path)
+		if len(_files) > 0:
+			for file in files:
+				print("file detected:%s", format(file))
+			exit()
+
+	def on_modified(self, event):
+		print "modified observer =", observer
+		print event.src_path
+		if os.path.exists(event.src_path):
+			file_stopped = 0
+			while file_stopped == 0:
+				size1 = os.path.getsize(event.src_path)
+				print "File size:", size1		#debug
+				time.sleep(0.5)
+				size2 = os.path.getsize(event.src_path)
+				print "File size:", size2		#debug
+				if size1 == size2:
+					file_stopped = 1
+			if size2 > 0:
+				self.process(event)
+
+if __name__ == '__main__':
+	try:
+		observer = Observer()        #folder watchdog process to monitor wav folder for new files
+		observer.schedule(MyHandler(), path=wav_dir)
