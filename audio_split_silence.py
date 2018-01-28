b8c5e61d9699c0f80c42e7f947f3e238c369d056
pasdesignal <noiseisgood@gmail.com>
2018-01-28 19:00:46 +1300

    Created sox process script

---
+++ audio_split_silence.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#To Do:
+#Use sox to process .wav file 
+#Split .wav file based on silence
+#Possible alogithm: 
+#1. Split audio based on silence parameters
+#2. Test fragemnts for length starting at first fragment
+#3. If first fragment > 03:00, append second fragment and test again
+#4. Continue until appended fragemtn is very close to 03:00mins in length
+#5. save appended .wav file to appropriate folder with appropriate naming for transcode
+
+#sox original.wav new.wav silence 1 0.5 2% 1 2.0 2% : newfile : restart
