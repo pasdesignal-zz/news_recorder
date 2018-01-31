f0f9b0ba4da725772e4bc86a8cf26e623d475368
pasdesignal <noiseisgood@gmail.com>
2018-01-31 15:38:25 +1300

    rethtsf

--- watchdog_transcode.py
+++ watchdog_transcode.py
@@ -1,7 +1,9 @@
-
+#!/usr/bin/python
 
 ##to do:
 #if folder dont exist - create them!
+# eg: if not os.path.exists(dest_dkfrontend):
+    	#os.makedirs(dest_dkfrontend)
 #this script requires watchdog module - pip install watchdog
 
 from watchdog.events import PatternMatchingEventHandler  
@@ -10,11 +12,11 @@ import os
 import datetime
 import time
 import ffmpy
+import shutil
 
 wav_dir = (os.getcwd()+'/audio/wav/')
-loudnorm_string = '-af loudnorm=I=-14:TP=-3:LRA=11:print_format=json'
 temp_dir = (os.getcwd()+'/audio/temp/')
-
+loudnorm_string = '-af loudnorm=I=-14:TP=-3:LRA=11:print_format=json'
 
 class MyHandler(PatternMatchingEventHandler):
 	patterns = ["*.wav"]
@@ -24,9 +26,9 @@ class MyHandler(PatternMatchingEventHandler):
 		#print event.src_path, event.event_type         #debug
 		#everything here is what happens once the event is triggered
 		print "processing file(s) for loudness using FFMPEG..."
-		new_name = (temp_dir+(os.path.basename(event.src_path)))
-		print "new name:{}".format(new_name)
-		self.normalise((event.src_path), new_name)
+		self.temp_file = (temp_dir+(os.path.basename(event.src_path)))
+		print "new name:{}".format(self.temp_file)
+		self.normalise((event.src_path), self.temp_file)
 
 	def on_modified(self, event):
 		print "detected new file {}".format(event.src_path)
@@ -52,12 +54,30 @@ class MyHandler(PatternMatchingEventHandler):
 		ff.run()			
 
 	def replace(self, orig_file, new_file):
-		print "deleting original file {}".format(event.src_path)
+		print "deleting original file:{}".format(event.src_path)
+		if os.path.isfile(event.src_path): 
+			os.remove(wav_dir+file)
+			if not os.path.isfile(event.src_path):
+				print "file {} deleted".format(event.src_path)
+				print "replacing with processed file:{}".format()
+				shutil.copy(self.temp_file, event.src_path)
+				if os.path.isfile(event.src_path):
+					print "deleting temp file:{}".format(self.temp_file)
+					os.remove(self.temp_file)
+					if not os.path.isfile(self.temp_file):
+						print "file {} deleted".format(self.temp_file)
+						print "file operations complete..."
+		else:
+			print "no file found:{}".format(event.src_path)
 
 if __name__ == '__main__':
-	#try:
-	#test folder exists, if not make it!
-	#if os.path(wav_dir) 
+	try:
+	#test folders exists, if not make them!
+	if not os.path.exists(wav_dir):
+		os.makedirs(wav_dir)
+	if not os.path.exists(temp_dir):
+		os.makedirs(temp_dir)
+	#call and start Observer class
 	print "starting watchdog process observing new files..."
 	observer = Observer()        #folder watchdog process to monitor wav folder for new files
 	observer.schedule(MyHandler(), path=wav_dir)
@@ -65,8 +85,8 @@ if __name__ == '__main__':
 	while True:
 		print "waiting ..."
 		time.sleep(2)
-	#except Exception as e:
-	#	print "Error:".format(e)
-	#finally:
-	#	print "outta here..."
-	#	exit()
+	except Exception as e:
+		print "Error:".format(e)
+	finally:
+		print "outta here..."
+		exit()
