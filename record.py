b76885ed15b612a32947915dd9dbde49a93f74b0
pasdesignal <noiseisgood@gmail.com>
2018-02-05 19:04:28 +1300

    removed housekeeping for now

--- record.py
+++ record.py
@@ -16,20 +16,10 @@ date_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 filename = 'rnznews_'+date_time+'.wav'
 #'-c:a pcm_s24be -r:a 48000 -ac 2 -t 30'
 #is there a bug in the way protocol_whitelist is parsed? Last option always ignored!
+ffmpeg_globals = "-y -hide_banner -protocol_whitelist 'file,udp,rtp,https' -v warning"
+ffmpeg_record_string = "-c:a pcm_s24be -r:a 48000 -ac 2 -t 20:00"
 
-recorder = ffmpy.FFmpeg(global_options="-y -hide_banner -protocol_whitelist 'file,udp,rtp,https'",inputs={sdp_file : '-c:a pcm_s24be -r:a 48000 -ac 2 -t 20:00'},outputs={(wav_dir+filename) : None })
-
-def housekeeping():
-	print "housekeeping..."
-	files_wav= []
-	files_wav = os.listdir(wav_dir)
-	if len(files_wav) > 0:
-		print "removing existing .wav files:", files_wav
-		for file in  files_wav:
-			try:
-				os.remove(wav_dir+file)        
-			except OSError:
-				pass
+recorder = ffmpy.FFmpeg(global_options=ffmpeg_globals,inputs={sdp_file : ffmpeg_record_string},outputs={(wav_dir+filename) : None })
 
 if __name__ == '__main__':
 	try:
