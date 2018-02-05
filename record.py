723a344d260abfbe1173295f8b851ff817e281b9
pasdesignal <noiseisgood@gmail.com>
2018-02-05 19:39:29 +1300

    indentation

--- record.py
+++ record.py
@@ -30,7 +30,7 @@ def listen():
 	conn, addr = s.accept()
 	print 'Connection address:', addr
 	while 1:
-    	data = conn.recv(BUFFER_SIZE)
+		data = conn.recv(BUFFER_SIZE)
     	if not data: break
     	print "received data:", data
     	#conn.send(data)  # echo
