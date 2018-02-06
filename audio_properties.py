941e27bb3cf40c26b4f02c4212ac33dcd63dc7e6
pasdesignal <noiseisgood@gmail.com>
2018-02-06 16:38:10 +1300

    dust

--- audio_properties.py
+++ audio_properties.py
@@ -12,13 +12,13 @@ test_wav = os.getcwd()+'/test.wav'
 class get_properties():
 
 	def __init__(self, audio_file):
-		input_file = audio_file
-		string = '-v quiet -show_format -show_streams -pretty -print_format json' #ffprobe input string
+		self.input_file = audio_file
+		self.string = '-v quiet -show_format -show_streams -pretty -print_format json' #ffprobe input string
 		timestamp = ''
 
 	#how to make this return an object describing the audio file properties?
 	def properties(self):		#testing for metadata
-		self.timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
+		timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
 		print "{} getting metadata of file: {}".format(self.timestamp, self.input_file)
 		ff = ffmpy.FFprobe(global_options = '-hide_banner -loglevel warning', inputs = {self.input_file: self.string})
 		try:
