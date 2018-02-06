3bf72fb3ed045a28223c9fcb4ec15ca6264819e0
pasdesignal <noiseisgood@gmail.com>
2018-02-06 17:28:51 +1300

    added test for each kind of audio file

--- audio_properties.py
+++ audio_properties.py
@@ -6,17 +6,21 @@ import ffmpy
 import os
 
 test_wav = os.getcwd()+'/test.wav'
+test_ogg = os.getcwd()+'/test.ogg'
+test_mp3 = os.getcwd()+'/test.mp3'
+test_opus = os.getcwd()+'/test.opus'
 
 class get_properties():
 
-	def __init__(self, audio_file):
-		self.input_file = audio_file
+	def __init__(self):
+		self.input = ''
 		self.string = '-v quiet -show_format -show_streams -pretty -print_format json' #ffprobe input string
 
 	#how to make this return an object describing the audio file properties?
-	def properties(self):		#testing for metadata
-		print "getting metadata of file: {}".format(self.input_file)
-		ff = ffmpy.FFprobe(global_options = '-hide_banner -loglevel warning', inputs = {self.input_file: self.string})
+	def properties(self, audio_file):		#testing for metadata
+		self.input = audio_file
+		print "getting metadata of file: {}".format(self.input)
+		ff = ffmpy.FFprobe(global_options = '-hide_banner -loglevel warning', inputs = {self.input: self.string})
 		try:
 			ff.run()	
 		except Exception as e:
@@ -25,5 +29,8 @@ class get_properties():
 			pass
 
 if __name__ == '__main__':
-	stats = get_properties(test_wav)
-	stats.properties()
+	stats = get_properties()
+	stats.properties(test_wav)
+	stats.properties(test_ogg)
+	stats.properties(test_mp3)
+	stats.properties(test_opus)
