36a8fbf8ad5993276c9267223bd705224e53f593
pasdesignal <noiseisgood@gmail.com>
2018-02-06 16:21:04 +1300

    kjkkl;

--- audio_properties.py
+++ audio_properties.py
@@ -34,5 +34,6 @@ if __name__ == '__main__':
 	except Exception as e:
 		print "Error:".format(e)
 	finally:
+		timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
 		print "Terminated {}.".format(timestamp)
 		exit()
