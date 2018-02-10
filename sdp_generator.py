6e92af2d18cb533877dce14fcde07e55e9506578
pasdesignal <noiseisgood@gmail.com>
2018-02-10 18:43:37 +1300

    self generating sdp file

--- sdp_generator.py
+++ sdp_generator.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
+#to do:
+#get local device ip address for sdp file? Required really?
+
 import socket
 import struct
 import time
-
 
 class SDP_Generator():
 
@@ -20,37 +22,47 @@ class SDP_Generator():
 		#print "address:", hex(self.multicastAddr)
 
 	#borrowed from https://github.com/SythilTech/Python-SDP/blob/master/scripts/sdp.py
-	def generate_sdp(self, ip, audio_port, rtp_profiles, session_description=" "):
+	def generate_sdp(self, session_description=""):
 	    sdp = ""
 	    #Protocol Version ("v=") https://tools.ietf.org/html/rfc4566#section-5.1 (always 0 for us)
 	    sdp += "v=0\r\n"
 	    #Origin ("o=") https://tools.ietf.org/html/rfc4566#section-5.2
-	    username = "-"
+	    username = "Recorder"
 	    sess_id = int(time.time())
 	    sess_version = 0
 	    nettype = "IN"
 	    addrtype = "IP4"
-	    sdp += "o=" + username + " " + str(sess_id) + " " + str(sess_version) + " " + nettype + " " + addrtype + " " + ip + "\r\n"
+	    sdp += "o=" + username + " " + str(sess_id) + " " + str(sess_version) + " " + nettype + " " + addrtype + " " + self.multicastaddr + "\r\n"
 	    #Session Name ("s=") https://tools.ietf.org/html/rfc4566#section-5.3
 	    sdp += "s=" + session_description + "\r\n"
-	    #Connection Information ("c=") https://tools.ietf.org/html/rfc4566#section-5.7
-	    sdp += "c=" + nettype + " " + addrtype + " " + ip + "\r\n"
 	    #Timing ("t=") https://tools.ietf.org/html/rfc4566#section-5.9
 	    sdp += "t=0 0\r\n"
+	    sdp += "a=type:multicast\r\n"
+	    #Connection Information ("c=") https://tools.ietf.org/html/rfc4566#section-5.7
+	    sdp += "c=" + nettype + " " + addrtype + " " + self.multicastaddr + "\r\n"
+	    
+	    
+	    
 	    #Media Descriptions ("m=") https://tools.ietf.org/html/rfc4566#section-5.14
-	    sdp += "m=audio " + str(audio_port) + " RTP/AVP"
-	    for rtp_profile in rtp_profiles:
-	        sdp += " " + str(rtp_profile)
+	    sdp += "m=audio " + '5004' + " RTP/AVP"
+	    sdp += " " + '96'
 	    sdp += "\r\n"
-	    sdp += "a=sendrecv\r\n"
+	    sdp += "a=rtpmap:96 L24/48000/2\r\n"
 	    self.sdp = sdp	
 
 if __name__ == '__main__':
 	sdp_object = SDP_Generator(4263)
 	print "address:", sdp_object.multicastaddr
-	sdp_object.generate_sdp(ip='172.17.2.69', audio_port=5004, rtp_profiles=['test'])
+	sdp_object.generate_sdp(session_description='RNZ Bulletin')
 	print ""
-	f = open('source.sdp')
+	filename = 'source.sdp'
+	f = open(filename, 'w')
+	print "writing sdp object to file:{}".format(filename)
+	f.write(sdp_object.sdp)
+	f = open(filename)
+	print ""
 	print f.read()
-	print""
-	print sdp_object.sdp
+	f = open('test.sdp')
+	print ""
+	print f.read()
+
