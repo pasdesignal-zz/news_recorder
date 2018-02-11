3ff992e3434cb40e566aeedebad551fcfd83145f
pasdesignal <noiseisgood@gmail.com>
2018-02-11 20:35:26 +1300

    duration testing

--- audio_trim.py
+++ audio_trim.py
@@ -23,22 +23,40 @@ class silence_trimmer():
 				os.makedirs(os.path.dirname(self.temp))
 
 	def trim_start(self, _input, _output):
-		print "trimming silence from start of file..."
+		print "trimming silence from start of file: {}".format(_input)
+		self.get_duration(_input)
+		time1 = self.duration
 		tfm = sox.Transformer()
 		tfm.silence(location=1, silence_threshold=0.1, min_silence_duration=0.5, buffer_around_silence=True)
 		tfm.build(_input, _output)
 		if os.path.isfile(_output):
-			print "trim_start success...maybe?"
+			self.get_duration(_output)
+			time2 = self.duration
+			if time1 => time2:
+				print "No silence removed"
+			else:
+				print "{} secs removed".format(time1-time2)
+		else:
+			print "ERROR: no output file detected"			
 
 	def trim_end(self, _input, _output):
-		print "trimming silence from end of file..."
+		print "trimming silence from end of file: {}".format(_input)
+		self.get_duration(_input)
+		time1 = self.duration
 		tfm = sox.Transformer()
 		tfm.reverse()
 		tfm.silence(location=1, silence_threshold=0.1, min_silence_duration=0.5, buffer_around_silence=True)
 		tfm.reverse()
 		tfm.build(_input, _output)
 		if os.path.isfile(_output):
-			print "trim end success...maybe?"		
+			self.get_duration(_output)
+			time2 = self.duration
+			if time1 => time2:
+				print "No silence removed"
+			else:
+				print "{} secs removed".format(time1-time2)
+		else:
+			print "ERROR: no output file detected"		
 
 	def get_duration(self, _file):
 		self.duration = sox.file_info.duration(_file)
@@ -46,28 +64,10 @@ class silence_trimmer():
 if __name__ == '__main__':
 	test_wav = (os.getcwd()+'/audio/test/test_bulletin.wav')
 	processed_wav = (os.getcwd()+'/audio/test/processed_bulletin.wav')
-	print "opening file:{}".format(test_wav)
+	print "opening file:{} for silence trimming".format(test_wav)
 	sox_object = silence_trimmer()
-	sox_object.get_duration(test_wav)
-	time1 = sox_object.duration
-	print "Duration before:{} secs".format(time1)
-	print "trimming silence off start of bulletin"
 	sox_object.trim_start(test_wav, sox_object.temp)
-	sox_object.get_duration(sox_object.temp)
-	time2 = sox_object.duration
-	print "Duration after trim_start:{} secs".format(time2)
-	if time2 >= time1:
-		print "Nothing trimmed!"
-	else:
-		print "{} secs removed".format(time1-time2)	
 	print "trimming silence off end of bulletin"
 	sox_object.trim_end(sox_object.temp, processed_wav)
-	sox_object.get_duration(processed_wav)
-	time3 = sox_object.duration
-	print "Duration after trim_end:{} secs".format(time3)
-	if time3 >= time2:
-		print "Nothing trimmed!"
-	else:
-		print "{} secs removed".format(time2-time3)	
 
 		
