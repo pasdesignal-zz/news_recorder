b4ff97ae149b167df3db23b5e90e482888e30537
pasdesignal <noiseisgood@gmail.com>
2018-02-17 17:44:34 +1300

    replace files after valid ok

--- bulletin_recorder.py
+++ bulletin_recorder.py
@@ -24,23 +24,24 @@ from audio_transcode import transcoder
 #housekeeping: delete all filetypes generated older than 24 hours: xml, wav, ogg, mp3, temp etc
 #make listen socket safer? Will basically parse anything?
 
-class bulletin_object(): #basic object to use for duration of bulletin creation
+class bulletin_object(): 					#basic object to use for duration of bulletin creation
 
 	def __init__(self):
-		timestamp = datetime.datetime.now() #get current time
-		timestamp_plus = timestamp + datetime.timedelta(minutes=10)	#bring time into next hour because we start early
+		timestamp = datetime.datetime.now()
+		timestamp_plus = timestamp + datetime.timedelta(minutes=10)				#bring time into next hour cause we start early
 		self.time = timestamp_plus.replace( minute=00, second=0, microsecond=0) #round down to nearest hour
 
 if __name__ == '__main__':
 	try:
 		##--SESSION VARIABLES--##
-		livewire_channel = 4263 			#4004 for testing #4263 for bulletins (Auckland)
+		livewire_channel = 4263 			#4004 for testing #4263 for bulletins (Auckland Livewire)
 		sdp_filename = 'source.sdp'
 		bind_interface = '10.212.13.1'		#for socket strings from Pathfinder
 		bind_port=5119						#for socket strings from Pathfinder
 		wav_dir = (os.getcwd()+'/audio/wav/')
 		mp3_dir = (os.getcwd()+'/audio/mp3/')
 		ogg_dir = (os.getcwd()+'/audio/ogg/')
+		#test for folders here
 		timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
 		template_xml = (os.getcwd()+'/diginews_template.xml')
 		podcast_baseurl = 'http://podcast.radionz.co.nz/news/'
@@ -112,25 +113,25 @@ if __name__ == '__main__':
 			timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
 			print "{} FATAL ERROR: invalid .wav file detected. Cannot continue: {}".format(timestamp, bulletin.filepath)
 			exit()
-		bulletin.xml.duration = bulletin.properties.duration	#add audio duration to xml	
+		bulletin.xml.duration = bulletin.properties.duration		#add audio duration to xml	
 		##--NORMALISE LOUDNESS--##
 		louder = loudness_normaliser(bulletin.filepath)
-		louder.normalise(louder.temp) 							#process orginal file and save as new file
-		louder.replace(bulletin.filepath, louder.temp) 			#replace orginal file with new file
+		louder.normalise(louder.temp) 								#process orginal file and save as new file
 		##--VALIDATE--##
 		print "testing for valid audio file after loudness process..."
-		bulletin.properties = get_properties(bulletin.filepath)
+		bulletin.properties = get_properties(louder.temp)
 		if bulletin.properties.valid == 1:
-			print "PASSED: .wav valid test OK: {}".format(bulletin.filepath)
+			print "PASSED: .wav valid test OK: {}".format(louder.temp)
+			louder.replace(bulletin.filepath, louder.temp) 			#replace orginal file with new file
 		else:
 			timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
-			print "{} FATAL ERROR: invalid .wav file detected. Cannot continue: {}".format(timestamp, bulletin.filepath)
+			print "{} FATAL ERROR: invalid .wav file detected. Cannot continue: {}".format(timestamp, louder.temp)
 			exit()
 		##--TRANSCODE--##
 		bulletin.transcoder = transcoder(bulletin.filepath)
 		bulletin.transcoder.transcode_mp3(bulletin.mp3_filepath)
 		bulletin.transcoder.transcode_ogg(bulletin.ogg_filepath)
-		##--VALIDATE MP3--##											#should this be folded into module?
+		##--VALIDATE MP3--##										#should this be folded into module?
 		#print "testing for valid mp3 audio file after transcode..."
 		#bulletin.mp3valid =bulletin.properties.valid
 		if bulletin.transcoder.mp3valid == 1:
@@ -141,7 +142,6 @@ if __name__ == '__main__':
 			print "ERROR: mp3 valid test BAD: {}".format(bulletin.mp3_filepath)
 		##--VALIDATE OGG--##
 		#print "testing for valid ogg audio file after transcode..."
-		
 		#bulletin.oggvalid =bulletin.properties.valid
 		if bulletin.transcoder.oggvalid == 1:
 			print "PASSED: ogg valid test OK: {}".format(bulletin.ogg_filepath)
