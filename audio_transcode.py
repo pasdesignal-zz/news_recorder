930a5f16735cda4db8f091e8a6c7a4fc270d655d
pasdesignal <noiseisgood@gmail.com>
2018-02-20 14:33:10 +1300

    matter of trust

--- audio_transcode.py
+++ audio_transcode.py
@@ -16,38 +16,6 @@ class transcoder():
 
 	def __init__(self, wav_in):
 		self.input = wav_in
-<<<<<<< HEAD
-		self.mp3_string = ('-map 0:0 -ac 1 -b:a 64k \
-		-metadata "Album=News Bulletin" \
-		-metadata "Track name=Radio New Zealand News" \
-		-metadata "Performer=Radio New Zealand" \
-		-metadata "Comment=News bulletin recorded at {}"').format(str(datetime.datetime.now().strftime("%-I%p")))
-		self.ogg_string = ('-map 0:0 -ac 1 -b:a 64k \
-		-metadata "Album=News Bulletin" \
-		-metadata "Track name=Radio New Zealand News" \
-		-metadata "Performer=Radio New Zealand" \
-		-metadata "Description=News bulletin recorded at {}"').format(str(datetime.datetime.now().strftime("%-I%p")))
-		self.ffmpeg_globals = '-y -hide_banner -v info'
-
-	def transcode_mp3(self, out_filename):
-		if os.path.exists(self.input):
-			if not os.path.isdir(os.path.dirname(out_filename)):
-				print "creating mp3 folder:{}".format(os.path.dirname(out_filename))
-				os.makedirs(os.path.dirname(out_filename))	
-			timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
-			print "{} initiating ffmpeg transcode process to mp3".format(timestamp)
-			mp3_filename = out_filename
-			ff = ffmpy.FFmpeg(global_options=self.ffmpeg_globals, inputs={self.input : None}, outputs={mp3_filename : self.mp3_string})
-			print ff.cmd
-			try:
-				ff.run()
-				timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
-				print "{} COMPLETE: ffmpeg mp3 transcoded file: {}".format(timestamp, mp3_filename)		
-			except ffmpy.FFRuntimeError as e:
-				print "ERROR: ffmpeg mp3 transcode processing: {}".format(e)
-			finally:
-				pass
-=======
 		if os.path.exists(self.input):
 			self.mp3_string = ('-map 0:0 -ac 1 -b:a 48k \
 			-metadata "Album=News Bulletin" \
@@ -60,7 +28,6 @@ class transcoder():
 			-metadata "Performer=Radio New Zealand" \
 			-metadata "Description=News bulletin recorded at {}"').format(datetime.datetime.now().strftime("%-I%p"))
 			self.ffmpeg_globals = '-y -hide_banner -v quiet'
->>>>>>> c9c04573c88c4fa7a1359ca28fbf8acfe4fdec4c
 		else:
 			print "FATAL ERROR: no transcode input file found: {}".format(self.input)
 
@@ -85,27 +52,6 @@ class transcoder():
 			pass
 	
 	def transcode_ogg(self, out_filename):
-<<<<<<< HEAD
-		if os.path.exists(self.input):
-			if not os.path.isdir(os.path.dirname(out_filename)):
-				print "creating mp3 folder:{}".format(os.path.dirname(out_filename))
-				os.makedirs(os.path.dirname(out_filename))	
-			timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
-			print "{} initiating ffmpeg transcode process to ogg".format(timestamp)
-			ogg_filename = out_filename
-			ff = ffmpy.FFmpeg(global_options=self.ffmpeg_globals, inputs={self.input : None}, outputs={ogg_filename : self.ogg_string})
-			print ff.cmd
-			try:
-				ff.run()	
-				timestamp = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
-				print "{} COMPLETE: ffmpeg mp3 transcoded file: {}".format(timestamp, ogg_filename)
-			except ffmpy.FFRuntimeError as e:
-				print "ERROR: ffmpeg ogg transcode processing: {}".format(e)
-			finally:
-				pass		
-		else:
-			print "ERROR: no file found: {}".format(self.wav_in)		
-=======
 		self.oggvalid = 0
 		if not os.path.isdir(os.path.dirname(out_filename)):
 			print "creating ogg folder:{}".format(os.path.dirname(out_filename))
@@ -123,8 +69,7 @@ class transcoder():
 		except ffmpy.FFRuntimeError as e:
 			print "ERROR: ffmpeg loudness processing: {}".format(e)
 		finally:
-			pass		
->>>>>>> c9c04573c88c4fa7a1359ca28fbf8acfe4fdec4c
+			pass
 
 	def housekeeping(self):
 		print "removing source file:{}".format(self.input)
