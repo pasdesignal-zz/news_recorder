fb3a948bca92b8cf23c46052b533251cfebc0a68
pasdesignal <noiseisgood@gmail.com>
2018-02-20 13:59:37 +1300

    indentation fun and games

--- web_publisher.py
+++ web_publisher.py
@@ -8,18 +8,18 @@ class check_ssh(): #object to use for duration of bulletin creation
 
 	def __init__(self):
 		timestamp = datetime.datetime.now() #get current time
-		timestamp_plus = timestamp + datetime.timedelta(minutes=10)	#bring time into next hour because we start early
+		timestamp_plus = timestamp + datetime.timedelta(minutes=10) #bring time into next hour because we start early
 		self.time = timestamp_plus.replace( minute=00, second=0, microsecond=0) #round down to nearest hour
 
 def check_it(self, ip, user, key_file, initial_wait=0, interval=0, retries=1):
-    ssh = paramiko.SSHClient()
+	ssh = paramiko.SSHClient()
 	try:
-	    ssh.connect(ip, username=user, key_filename=key_file)
-	    return True
+		ssh.connect(ip, username=user, key_filename=key_file)
+		return True
 	except (BadHostKeyException, AuthenticationException, 
-	        SSHException, socket.error) as e:
-	    print e
-	    sleep(interval)
+			SSHException, socket.error) as e:
+		print e
+		sleep(interval)
 
 if __name__ == '__main__':
 	try:
