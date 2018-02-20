6756b87836289b5095b1fe6b6e43672d3dde74bc
pasdesignal <noiseisgood@gmail.com>
2018-02-20 16:06:35 +1300

    classy bitches

--- web_publisher.py
+++ web_publisher.py
@@ -12,30 +12,33 @@ import os
 
 class check_ssh(): #object to use for duration of bulletin creation
 
-	def __init__(self, sock):
-		self.sock = sock
+	def __init__(self, dest, port, user, key_file):
+		self.dest = dest
+		self.port = port
+		self.user =user
+		self.key_file = key_file
+		proxy_uri = "http://172.17.8.1:3128"
+		url = urlparse.urlparse(proxy_uri)
+		http_con = httplib.HTTPConnection(url.hostname, url.port)
+		headers = {}	
+		http_con.set_tunnel(self.dest, self.port, headers)
+		http_con.connect()
+		self.sock = http_con.sock
 
-	def check_it(self, ip, user, key_file, initial_wait=0, interval=0, retries=1):
+	def check_it(self, initial_wait=0, interval=0, retries=1):
 		ssh = paramiko.SSHClient()
 		ssh.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
-		print "testing SSH connectivity to {} as user {}".format(ip, user)
-		ssh.connect(ip, username=user, key_filename=key_file, sock=self.sock)
+		print "testing SSH connectivity to {} as user {}".format(self.dest, self.user)
+		ssh.connect(self.dest, username=self.user, key_filename=self.key_file, sock=self.sock)
 		return True
 
 if __name__ == '__main__':
 	elf_staging = '150.242.42.149'
+	elf_port = 22
 	elf_user = 'deploy'
 	key_file = '/home/deploy/.ssh/id_rsa.pub'
-	port = 22
-	proxy_uri = "http://172.17.8.1:3128"
-	url = urlparse.urlparse(proxy_uri)
-	http_con = httplib.HTTPConnection(url.hostname, url.port)
-	headers = {}	
-	http_con.set_tunnel(elf_staging, port, headers)
-	http_con.connect()
-	sock = http_con.sock
-	test = check_ssh(sock)
-	result = test.check_it(elf_staging, elf_user, key_file)
+	test = check_ssh(elf_staging, elf_port, elf_user, key_file)
+	result = test.check_it()
 	if result == True:
 		print "success bitches!!!"
 	else:
